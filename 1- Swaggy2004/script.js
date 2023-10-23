5e6447a5abd4fb23f959f5bf65f57d0e5d77c76d
Swaggy <101066701+swaggy2004@users.noreply.github.com>
2023-10-23 13:15:42 +0530

    Implemented the quantity calculator for the seocnd form

--- 1- Swaggy2004/script.js
+++ 1- Swaggy2004/script.js
@@ -1,5 +1,7 @@
 var totGuns = 1;
+var totInv = 0;
 var totprice = 0;
+var totprice2= 0;
 function getQuantity()
 {
     totGuns = document.getElementById("qty-1").value;
@@ -7,14 +9,34 @@ function getQuantity()
     document.getElementById("price").innerHTML = "$" + totprice;
 }
 
+function getQuantity2()
+{
+    totInv = document.getElementById("qty-2").value;
+    totprice2 = 0;
+    document.getElementById("price2").innerHTML = "$" + totprice2;
+}
+
 function calculatePrice(price)
 {
-    var gun = document.getElementById.value;
     if (totGuns > 0)
     {
         totprice += price;
         totGuns--;
         document.getElementById("price").innerHTML = "$" + totprice;
+    }
+    else
+    {
+        alert("You maxed out on the quantity!");    
+    }
+}
+
+function calculatePrice2(price)
+{
+    if (totInv > 0)
+    {
+        totprice2 += price;
+        totInv--;
+        document.getElementById("price2").innerHTML = "$" + totprice2;
     }
     else
     {
