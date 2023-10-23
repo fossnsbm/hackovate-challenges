2f7b067e7b4986e57518d352c7ca02cd140658f6
Swaggy <101066701+swaggy2004@users.noreply.github.com>
2023-10-23 12:57:15 +0530

    Added the calculating functionality for the guns

--- 1- Swaggy2004/script.js
+++ 1- Swaggy2004/script.js
@@ -1,6 +1,25 @@
-var totGuns = 0;
+var totGuns = 1;
+var totprice = 0;
 function getQuantity()
 {
     totGuns = document.getElementById("qty-1").value;
     window.alert(totGuns);
+    totprice = 0;
+    document.getElementById("price").innerHTML = "$" + totprice;
 }
+
+function calculatePrice(price)
+{
+    var gun = document.getElementById.value;
+    if (totGuns > 0)
+    {
+        totprice += price;
+        totGuns--;
+        document.getElementById("price").innerHTML = "$" + totprice;
+    }
+    else
+    {
+        alert("You maxed out on the quantity!");    
+    }
+}
+
