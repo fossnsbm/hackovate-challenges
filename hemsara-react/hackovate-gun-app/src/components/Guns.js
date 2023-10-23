68c7618359cd887b56d3fb17a51bf9925c336c58
Vehan Hemsara <79366878+Hemsara@users.noreply.github.com>
2023-10-23 12:18:09 +0530

    feature 2

--- hemsara-react/hackovate-gun-app/src/components/Guns.js
+++ hemsara-react/hackovate-gun-app/src/components/Guns.js
@@ -1,28 +1,52 @@
 import "../index.scss";
 
-function Guns({ guns }) {
-  console.log(guns);
+function Guns({ guns, qty, selectedGuns, setGunQTY, setSelectedGuns }) {
+  function addItem(gun) {
+    if (selectedGuns.length >= qty) {
+      alert("Quantity limit exceeded");
+      return;
+    }
+    let contains = selectedGuns.some((item) => item.name === gun.name);
+    if (!contains) {
+      setSelectedGuns([...selectedGuns, gun]);
+    } else {
+      setSelectedGuns(selectedGuns.filter((item) => item.name !== gun.name));
+    }
+  }
   return (
     <div className="guns">
       <div className="qty_select">
         <h4>Select gun quantity</h4>
-        <select name="guns" id="guns" className="dropdown">
-          <option value="">1</option>
-          <option value="">2</option>
-          <option value="">3</option>
-          <option value="">4</option>
-          <option value="">5</option>
-          <option value="">6</option>
-          <option value="">7</option>
-          <option value="">8</option>
-          <option value="">9</option>
-          <option value="">10</option>
+        <select
+          name="guns"
+          id="guns"
+          className="dropdown"
+          onChange={(e) => setGunQTY(e.target.value)}
+        >
+          <option value="1">1</option>
+          <option value="2">2</option>
+          <option value="3">3</option>
+          <option value="4">4</option>
+          <option value="5">5</option>
+          <option value="6">6</option>
+          <option value="7">7</option>
+          <option value="8">8</option>
+          <option value="9">9</option>
+          <option value="10">10</option>
         </select>
       </div>
       <div className="gun_list">
         <ul>
           {guns.map((gun, index) => (
-            <div key={index} className="product">
+            <div
+              onClick={() => addItem(gun)}
+              key={index}
+              className={`product ${
+                selectedGuns.some((item) => item.name === gun.name)
+                  ? "selected"
+                  : ""
+              }`}
+            >
               <span>{gun.name}</span>
             </div>
           ))}
@@ -30,7 +54,13 @@ function Guns({ guns }) {
       </div>
       <div className="gun_total">
         <h4>Gun subtotal</h4>
-        <h4>USD 456O.00</h4>
+        <h4>
+          USD
+          {selectedGuns.reduce(function (prev, current) {
+            return prev + current.price;
+          }, 0)}
+          .00
+        </h4>
       </div>
     </div>
   );
--- hemsara-react/hackovate-gun-app/src/components/Inventories.js
+++ hemsara-react/hackovate-gun-app/src/components/Inventories.js
@@ -1,27 +1,63 @@
 import "../index.scss";
 
-function Inventories({ inventories }) {
+function Inventories({
+  inventories,
+  qty,
+  selectedInventories,
+  setInventoryQTY,
+  setSelectedInventory,
+}) {
+  console.log(qty);
+  function addItem(inventory) {
+    if (selectedInventories.length >= qty) {
+      alert("Quantity limit exceeded");
+      return;
+    }
+    let contains = selectedInventories.some(
+      (item) => item.name === inventory.name
+    );
+    if (!contains) {
+      setSelectedInventory([...selectedInventories, inventory]);
+    } else {
+      setSelectedInventory(
+        selectedInventories.filter((item) => item.name !== inventory.name)
+      );
+    }
+  }
   return (
     <div className="inventories">
       <div className="qty_select">
         <h4>Select inventory quantity</h4>
-        <select name="inventory" id="inventory" className="dropdown">
-          <option value="">1</option>
-          <option value="">2</option>
-          <option value="">3</option>
-          <option value="">4</option>
-          <option value="">5</option>
-          <option value="">6</option>
-          <option value="">7</option>
-          <option value="">8</option>
-          <option value="">9</option>
-          <option value="">10</option>
+        <select
+          name="inventory"
+          id="inventory"
+          className="dropdown"
+          onChange={(e) => setInventoryQTY(e.target.value)}
+        >
+          <option value="1">1</option>
+          <option value="2">2</option>
+          <option value="3">3</option>
+          <option value="4">4</option>
+          <option value="5">5</option>
+          <option value="6">6</option>
+          <option value="7">7</option>
+          <option value="8">8</option>
+          <option value="9">9</option>
+          <option value="10">10</option>
         </select>
       </div>
       <div className="gun_list">
         <ul>
           {inventories.map((inventory, index) => (
-            <div key={index} className="product">
+            <div
+              onClick={() => addItem(inventory)}
+              key={index}
+              className={`product ${
+                selectedInventories.some((item) => item.name === inventory.name)
+                  ? "selected"
+                  : ""
+              }`}
+            >
               <span>{inventory.name}</span>
             </div>
           ))}
@@ -29,7 +65,13 @@ function Inventories({ inventories }) {
       </div>
       <div className="gun_total">
         <h4>Inventory subtotal</h4>
-        <h4>USD 458O.00</h4>
+        <h4>
+          USD{" "}
+          {selectedInventories.reduce(function (prev, current) {
+            return prev + current.price;
+          }, 0)}
+          .00
+        </h4>
       </div>
     </div>
   );
