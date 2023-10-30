e57d690e403b8dbb4510f24dbc24f29f6d164878
Yasiru Dharmathilaka <yasiru.dharmathilaka@gmail.com>
2023-10-30 19:25:06 +0530

    Issue #2 - Implemented Selection of Menu Items

--- components/index/ItemMenu.tsx
+++ components/index/ItemMenu.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { ChangeEvent } from "react";
 
 import styles from "./ItemMenu.module.scss";
 
@@ -7,12 +7,13 @@ interface Item {
   name: string;
   price: number;
   quantity: number;
+  selected: boolean;
 }
 
 function ItemMenu({ items }: { items: Item[] }) {
-  const [order, setOrder] = React.useState<Item[]>([]);
-
-  const [ITEMS, setItems] = React.useState<Item[]>(items);
+  const [menuItems, setItems] = React.useState<Item[]>(items);
+  const [menuSubTotal, setMenuSubTotal] = React.useState<number>(0);
+  const [menuItemCap, setMenuItemCap] = React.useState<number>(0);
 
   const [hydrated, setHydrated] = React.useState<boolean>(false);
 
@@ -23,36 +24,100 @@ function ItemMenu({ items }: { items: Item[] }) {
     return null;
   }
 
-  function addToOrder(item: Item, mode: number) {
+  function selectItem(e: ChangeEvent, item: Item) {
+    var totalSelected = 0;
+    const selected = !item.selected;
+
+    if (!selected) {
+      item.quantity = 0;
+    }
+
+    menuItems.forEach((i) => {
+      if (i.selected) {
+        totalSelected++;
+      }
+    });
+
+    if (totalSelected >= menuItemCap && !item.selected) {
+      alert("You have reached the limit of items you can select!");
+      e.preventDefault();
+      return;
+    }
+
+    const newMenuItems = menuItems.map((i) => {
+      if (i.i === item.i) {
+        i = { ...i, selected: !i.selected };
+        return i;
+      }
+
+      return i;
+    });
+
+    setItems(newMenuItems);
+  }
+
+  function refreshSubTotal(items: Item[]) {
     return;
   }
 
-  function getSubTotal(items: Item[]) {
-    return 0;
+  function addToOrder(item: Item, mode: number) {
+    const newMenuItems = menuItems.map((i) => {
+      if (i.i === item.i) {
+        if (!i.selected) {
+          alert("Please select the item by toggling the checkbox!");
+          return i;
+        }
+
+        i = { ...i, quantity: i.quantity + mode };
+        if (i.quantity < 0) {
+          return { ...i, quantity: 0 };
+        }
+        return i;
+      }
+
+      return i;
+    });
+
+    setItems(newMenuItems);
+    refreshSubTotal(newMenuItems);
   }
 
   return (
     <div className={styles.ItemMenu}>
       <table>
         <tr>
-          <td colSpan={7}>
+          <td colSpan={8}>
             <b>Select Quantity</b>
           </td>
           <td>
             <b>
-              <input type='number' placeholder='0' />
+              <input
+                type='number'
+                placeholder='0'
+                onInputCapture={(e) => {
+                  e.target.value > 0 ? null : (e.target.value = 0);
+                  setMenuItemCap(e.target.value);
+                }}
+              />
             </b>
           </td>
         </tr>
 
         <tr>
-          <td colSpan={8}>
+          <td colSpan={9}>
             <hr />
           </td>
         </tr>
 
-        {ITEMS.map((item, i) => (
+        {menuItems.map((item, i) => (
           <tr key={i}>
+            <td>
+              <input
+                type='checkbox'
+                {...{ checked: item.selected }}
+                onChange={(e) => selectItem(e, item)}
+              />
+            </td>
             <td className={styles.Item}>{item.name}</td>
             <td>
               <button onClick={() => addToOrder(item, 1)}>+</button>
@@ -70,17 +135,17 @@ function ItemMenu({ items }: { items: Item[] }) {
         ))}
 
         <tr>
-          <td colSpan={8}>
+          <td colSpan={9}>
             <hr />
           </td>
         </tr>
 
         <tr>
-          <td colSpan={7}>
+          <td colSpan={8}>
             <b>Subtotal:</b>
           </td>
           <td className={styles.number}>
-            <b>${getSubTotal(items)}</b>
+            <b>${menuSubTotal}</b>
           </td>
         </tr>
       </table>
